946d5c96094dd806a9d6ef90a56f87f703c8fb2c
Marco Marchitelli <marchitelli97@libero.it>
2019-06-21 20:56:56 +0200

    Crash fix!!

--- Assets/Scripts/Infinite_Runner/EnemySpawner.cs
+++ Assets/Scripts/Infinite_Runner/EnemySpawner.cs
@@ -1,56 +1,63 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EnemySpawner : MonoBehaviour
+﻿namespace Rewind
 {
-    public bool spawnOnStart;
-    public GameObject[] prefabs;
-    public float startTime, endTime;
-    public AnimationCurve difficultyOverTime;
-    public float width;
+    using UnityEngine;
+    using System.Collections;
 
-    float timer, currentSpawnTime;
-
-    private void Start()
+    public class EnemySpawner : MonoBehaviour
     {
-        if (spawnOnStart)
+        public bool spawnOnStart;
+        public GameObject[] prefabs;
+        public float startTime, endTime;
+        public AnimationCurve difficultyOverTime;
+        public float width;
+
+        float timer, currentSpawnTime;
+
+        private void Start()
         {
-            StartSpawning();
+            RewindManager.Instance.OnRewindStart += StopSpawning;
+            RewindManager.Instance.OnRewindEnd   += StartSpawning;
+
+            if (spawnOnStart)
+            {
+                StartSpawning();
+            }
         }
-    }
 
-    void StartSpawning()
-    {
-        StartCoroutine("SpawnRoutine");
-    }
-
-    void StopSpawning()
-    {
-        StopCoroutine("SpawnRoutine");
-    }
-
-    IEnumerator SpawnRoutine()
-    {
-        timer = 0;
-        while (timer < currentSpawnTime)
+        void StartSpawning()
         {
             currentSpawnTime = Mathf.Lerp(startTime, endTime, difficultyOverTime.Evaluate(Time.time));
-            timer += Time.deltaTime;
-            yield return null;
+            StartCoroutine("SpawnRoutine");
         }
 
-        Spawn();
-    }
+        void StopSpawning()
+        {
+            StopCoroutine("SpawnRoutine");
+        }
 
-    void Spawn()
-    {
-        StopSpawning();
+        IEnumerator SpawnRoutine()
+        {
+            timer = 0;
+            while (timer < currentSpawnTime)
+            {
+                currentSpawnTime = Mathf.Lerp(startTime, endTime, difficultyOverTime.Evaluate(Time.time));
+                timer += Time.deltaTime;
+                yield return null;
+            }
 
-        int randomIndex = Random.Range(0, prefabs.Length);
-        Vector3 randomPos = new Vector3(Random.Range(transform.position.x - width * .5f, transform.position.x + width * .5f), transform.position.y, transform.position.z);
+            Spawn();
+        }
 
-        Instantiate(prefabs[randomIndex], randomPos, transform.rotation);
+        void Spawn()
+        {
+            StopSpawning();
 
-        StartSpawning();
+            int randomIndex = Random.Range(0, prefabs.Length);
+            Vector3 randomPos = new Vector3(Random.Range(transform.position.x - width * .5f, transform.position.x + width * .5f), transform.position.y, transform.position.z);
+
+            Instantiate(prefabs[randomIndex], randomPos, transform.rotation);
+
+            StartSpawning();
+        }
     }
 }
