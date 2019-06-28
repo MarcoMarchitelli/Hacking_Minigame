bf8796e2a25809cbf614c8c40e8df0cf8c9ce484
Marco Marchitelli <marchitelli97@libero.it>
2019-06-28 19:11:56 +0200

    Main menu coming out nicely!

---
+++ Assets/Scripts/Utility/PulseTweener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class PulseTweener : MonoBehaviour
+{
+    private const float SCALE_MULTIPLIER = 2;
+    private const float PULSE_DURATION = 1f;
+
+    private void Start()
+    {
+        Sequence pulsatingSequence = DOTween.Sequence();
+
+        pulsatingSequence.Append(transform.DOScale(SCALE_MULTIPLIER, PULSE_DURATION).SetEase(Ease.Linear));
+        pulsatingSequence.Append(transform.DOScale(1, PULSE_DURATION).SetEase(Ease.Linear));
+        pulsatingSequence.SetLoops(-1);
+    }
+}
