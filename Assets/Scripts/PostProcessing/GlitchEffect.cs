38b8ea9e7f14b29474134493e6f85b190aef06ac
Marco Marchitelli <marchitelli97@libero.it>
2019-06-28 15:27:37 +0200

    Imported glitch effect in project.

---
+++ Assets/Scripts/PostProcessing/GlitchEffect.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[System.Serializable]
+[PostProcess(typeof(GlitchEffectRenderer), PostProcessEvent.BeforeStack, "Custom/GlitchEffect")]
+public class GlitchEffect : PostProcessEffectSettings
+{
+    public FloatParameter ChromAberrAmountX = new FloatParameter();
+    public FloatParameter ChromAberrAmountY = new FloatParameter();
+    public FloatParameter RightStripesAmount = new FloatParameter();
+    public FloatParameter LeftStripesAmount = new FloatParameter();
+    [Range(0, 1)]
+    public FloatParameter RightStripesFill = new FloatParameter();
+    [Range(0,1)]
+    public FloatParameter LeftStripesFill = new FloatParameter();
+    public Vector2Parameter DisplacementAmount = new Vector2Parameter();
+    public FloatParameter WavyDisplFreq = new FloatParameter();
+    public FloatParameter Effect = new FloatParameter();
+}
---
+++ Assets/Scripts/PostProcessing/GlitchEffectRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Rendering.PostProcessing;
+using UnityEngine;
+
+public sealed class GlitchEffectRenderer : PostProcessEffectRenderer<GlitchEffect>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        Material mat = new Material(Shader.Find("Custom/GlitchEffectShader"));
+
+        mat.SetFloat("_ChromAberrAmountX", settings.ChromAberrAmountX);
+        mat.SetFloat("_ChromAberrAmountY", settings.ChromAberrAmountY);
+        mat.SetFloat("_RightStripesAmount", settings.RightStripesAmount);
+        mat.SetFloat("_RightStripesFill", settings.RightStripesFill);
+        mat.SetFloat("_LeftStripesAmount", settings.LeftStripesAmount);
+        mat.SetFloat("_LeftStripesFill", settings.LeftStripesFill);
+        mat.SetVector("_DisplacementAmount", settings.DisplacementAmount);
+        mat.SetFloat("_WavyDisplFreq", settings.WavyDisplFreq);
+        mat.SetFloat("_GlitchEffect", settings.Effect);
+
+        context.command.Blit(context.source, context.destination, mat);
+    }
+}
